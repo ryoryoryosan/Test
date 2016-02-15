24de6c7f5df5e9a5b252214a5a76625bdfbeeb22
ryoryoryosan <god.hand.clash@gmail.com>
2016-02-15 15:56:06 +0900

    FugaFuga

---
+++ Assets/Hoge.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hoge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		//FugaFuga
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
